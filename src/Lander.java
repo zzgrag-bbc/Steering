c34eacd941ffcf85eade7cc1c20eb67f90bfcda7
zzgrag-bbc <gian.zgraggen@bbcag.ch>
2016-05-10 14:28:00 +0200

    Improved Steering
    Improved left & right steering

--- src/Lander.java
+++ src/Lander.java
@@ -19,7 +19,7 @@ public class Lander extends Actor implements GGKeyListener {
 	private 		double 		accelerationY; 													// Acceleration in vertical direction
 	private 		int 		thrustLevel; 													// Thrust level
 	private 		double 		fuel; 															// Remaining fuel
-	private 		Actor 		ufoCrashed 		= new Actor("sprites/landerdebris.gif");		// Crashed Ufo Actor
+	private 		Actor 		ufoCrashed 		= new Actor("sprites/landerdebris.gif");		// Crashed UFO Actor
 	private 		Actor 		thrust 			= new Actor("sprites/thrust.gif", 9);			// Thrust Fire Actor
 	private 		boolean 	isLanded 		= false;										// Boolean for checking if UFO is already landed
 	private 		HiResTimer 	timer 			= new HiResTimer();								// Timer ?
@@ -27,11 +27,12 @@ public class Lander extends Actor implements GGKeyListener {
 	private 		boolean 	fuelExpired;													// Boolean for disabling controls if fuel is expired
 	
 	
+	// Constructor
 	public Lander() {
 		super("sprites/lander.gif");
 	}
 
-	
+	// Reset Method always called before starting
 	public void reset() {
 		
 		GameGrid gg = gameGrid;
@@ -64,29 +65,51 @@ public class Lander extends Actor implements GGKeyListener {
 	}
 
 	
+	// Will activate when doRun(); is called
+	// Contains most of the logic
 	public void act() {
 		
 		GameGrid gg = gameGrid;
 		double vdispy = (int) (100 * velocityY) / 100.0;
-		double hy = 490 - y;
+		double height = 490 - y;
+		
+		
+		// Making name of JFrame
 		String s;
-		
+		// Name of JFrame when fuel is empty
 		if (fuelExpired)
-			s = String.format("hy = %10.2f m    velocityY = %10.2f m/s    accelerationY = %10.2f m/s^2    fuel = %10.0f kg (expired)", hy,
-					velocityY, accelerationY, fuel);
-		
+			s = String.format(
+					"Höhe = %10.2f m    "
+					+ "Geschw. = %10.2f m/s    "
+					+ "Beschl. = %10.2f m/s^2    "
+					+ "Treibst. = %10.0f kg (expired)", 
+					height, 
+					velocityY, 
+					accelerationY, 
+					fuel
+					);
+		// Name of JFrame when fuel is not empty
 		else
-			s = String.format("hy = %10.2f m    velocityY = %10.2f m/s    accelerationY = %10.2f m/s^2    fuel = %10.0f kg", hy, velocityY, accelerationY,
-					fuel);
-		
+			s = String.format(
+					"height = %10.2f m    "
+					+ "velocity = %10.2f m/s    "
+					+ "acceleration = %10.2f m/s^2    "
+					+ "fuel = %10.0f kg", 
+					height, 
+					velocityY, 
+					accelerationY, 
+					fuel
+					);
 		gg.setTitle(s);
+		
+		
+		
 		double dtx = 2 * gg.getSimulationPeriod() / 1000.0; // Time scaled: * 2
 		double dty = 2 * gg.getSimulationPeriod() / 1000.0; // Time scaled: * 2
 		velocityX = velocityX + accelerationX * dtx;
 		velocityY = velocityY + accelerationY * dty;
 		x = x + velocityX * dtx;
 		y = y + velocityY * dty;
-		
 		setLocation(new Location((int) x, (int) y));
 		thrust.setLocation(new Location((int) x, (int) y + 57));
 		fuel = fuel - thrustLevel * fuelFactor;
@@ -101,8 +124,11 @@ public class Lander extends Actor implements GGKeyListener {
 			fuelExpired = true;
 		}
 
+		
+		// If UFO hit correct height level and landed
 		if (getLocation().y > 490 && !isLanded) {
 			gg.setTitle("Touchdown!");
+			// Sets Crashed UFO if UFO was to fast
 			if (velocityY > 10.0) {
 				ufoCrashed.setLocation(new Location(getLocation().x, getLocation().y + 30));
 				ufoCrashed.show();
@@ -110,7 +136,7 @@ public class Lander extends Actor implements GGKeyListener {
 				gg.getBg().drawText("Sorry! Crashed with speed: " + vdispy + " m/s", new Point(20, 300));
 				gg.playSound(this, GGSound.EXPLODE);
 			}
-			
+			// Displays stats if Velocity is 10.0 or beneath.
 			else {
 				long time = timer.getTime();
 				gg.getBg().drawText("Congratulation! Landed with speed: " + vdispy + " m/s", new Point(20, 300));
@@ -118,7 +144,7 @@ public class Lander extends Actor implements GGKeyListener {
 				gg.getBg().drawText("Remaining fuel: " + (int) fuel + " kg", new Point(20, 400));
 				gg.playSound(this, GGSound.FADE);
 			}
-			
+			// Re-Starting of Game
 			player.stop();
 			gg.getBg().drawText("Press any key...", new Point(20, 450));
 			setActEnabled(false);
@@ -139,8 +165,8 @@ public class Lander extends Actor implements GGKeyListener {
 			return true;
 		}
 
+		// Steering, set Thrust Level, set Acceleration
 		double da = 0.4;
-		
 		switch (evt.getKeyCode()) {
 		
 			case KeyEvent.VK_UP:
@@ -158,10 +184,16 @@ public class Lander extends Actor implements GGKeyListener {
 				break;
 				
 			case KeyEvent.VK_LEFT:
+				if ( accelerationX > 0 ) {
+					accelerationX = 0;
+				}
 				accelerationX -= da;
 				break;
 				
 			case KeyEvent.VK_RIGHT:
+				if ( accelerationX < 0 ) {
+					accelerationX = 0;
+				}
 				accelerationX += da;
 				break;
 		}
